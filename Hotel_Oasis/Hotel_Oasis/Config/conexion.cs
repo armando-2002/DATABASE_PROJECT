2526acfd6a915e37bd08b5e5aa1c02b722f9376b
Armando <125172812+armando-2002@users.noreply.github.com>
2025-02-01 08:41:50 -0500

    añadir dirección IP del servidor

--- Hotel_Oasis/Hotel_Oasis/Config/conexion.cs
+++ Hotel_Oasis/Hotel_Oasis/Config/conexion.cs
@@ -9,17 +9,33 @@ namespace Hotel_Oasis.Config
 {
     class conexion
     {
+        private static string connectionString = "Server=26.2.41.132;Database=Hotel_Quito;Trusted_Connection=true";
+
+
         public static SqlConnection Conectar()
         {
-            //SqlConnection conexion = new  SqlConnection("server=WIN-T0JD7FVAOU7;database=HotelDB;Trusted_Connection=true");
-            //conexion.Open();
-            ///// SqlConnection conexion = new SqlConnection("server=WIN-T0JD7FVAOU7;database=Hotel_Oasis_Quito;Trusted_Connection=true");
+            ////SqlConnection conexion = new  SqlConnection("server=WIN-T0JD7FVAOU7;database=HotelDB;Trusted_Connection=true");
             ////conexion.Open();
-            SqlConnection conexion = new SqlConnection("server=WIN-T0JD7FVAOU7;database=Hotel_Quito;Trusted_Connection=true");
-            conexion.Open();
+            /////// SqlConnection conexion = new SqlConnection("server=WIN-T0JD7FVAOU7;database=Hotel_Oasis_Quito;Trusted_Connection=true");
+            //////conexion.Open();
+            //SqlConnection conexion = new SqlConnection("server=WIN-T0JD7FVAOU7;database=Hotel_Quito;Trusted_Connection=true");
+            //conexion.Open();
 
-            return conexion;
-                }
+            //return conexion;
+            SqlConnection conexion = new SqlConnection(connectionString);
+
+            try
+            {
+                conexion.Open(); // Intenta abrir la conexión
+                Console.WriteLine("Conexión exitosa!");
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar: " + ex.Message);
+                return null; // Devuelve null si hay un error
+            }
+        }
 
 
     }
